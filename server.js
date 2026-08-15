0352cdd77d1e589c2316ec95e0bcdb0d0e230df1
SG-cloud-lab <blessedshemmy@gmail.com>
2026-08-15 03:05:21 +0200

    Refactor server.js for improved AI question handling

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 require("dotenv").config();
 const express = require("express");
+const askAI = require("./ai");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req,res)=>{
   res.send("DNFC Kingdom AI Backend is running");
-});
-
-app.post("/ask", (req, res) => {
-  const question = req.body.question;
-
-  res.json({
-    answer: "Your question was received: " + question
-  });
 });
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+app.post("/ask", async (req,res)=>{
+
+  const question = req.body.question;
+
+  const answer = await askAI(question);
+
+  res.json({
+    answer: answer
+  });
+
+});
+
+app.listen(PORT, ()=>{
   console.log(`DNFC Kingdom AI running on port ${PORT}`);
 });
